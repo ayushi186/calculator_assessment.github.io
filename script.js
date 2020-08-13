1f64ffa6d3b2a92f2b85dd896fc61fff6a77aba8
Ayushi <ayushi.karn186@gmail.com>
2020-08-13 13:20:52 +1000

    initial commit

---
+++ script.js
@@ -0,0 +1,122 @@
+const numberBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operation]');
+const equalsBtn = document.querySelector('[data-equals]');
+const allClearBtn = document.querySelector('[data-clear]');
+const displayElement = document.querySelector('[data-display]');
+
+
+class calculator {
+    constructor(displayElement){
+        this.displayElement = displayElement;
+        this.allClear();
+    }
+
+allClear(){
+ this.displayElement.innerText = '0';
+ this.currentOperand = '';
+ this.previousOperand = ''
+ this.operation = '';
+}
+
+appendNumberToDisplay(number){
+if(number === '.' && this.currentOperand.includes('.')) return;
+this.currentOperand = this.currentOperand.toString() + number.toString();
+
+}
+
+
+checkOperation(operation){
+    console.log(operation);
+/*there is no number passed fo before hitting the operator (+,-,*,÷) stop!*/
+     if(this.currentOperand === '')  return;
+/*below else if condition is used to check for concatenated operations like - 2+2=4*3=12-2  alongwith normal single calculation*/ 
+    else if (this.previousOperand !== '' ){
+        this.compute();
+    }
+/*clear off the currentOperand and populate previousOperand and operation when we only have the currentOperand but no previous cached value for calculation */
+     this.operation = operation;
+     this.previousOperand = this.currentOperand + this.operation;
+     console.log(this.previousOperand);
+     this.currentOperand = '';
+}
+
+/* This method is where the real calculation happen*/
+compute(){
+/*Initiate a variable that will hold the calculation value*/
+    let computation;
+/* Convert the previous and current operand into float for decimal point calculations */
+    const previous = parseFloat(this.previousOperand);
+    console.log(previous);
+    const current = parseFloat(this.currentOperand);
+    console.log(current);
+/* If the previous and current operands are not a number then Stop!*/
+    if(isNaN(previous) || isNaN(current)) return;
+/*otherwise check of the switch cases - go to the one and execute based on the operations variable*/
+    switch(this.operation){
+        case '+' :
+            computation = previous + current;
+            break;
+        case '-' :
+            computation = previous - current;
+            break;
+        case 'x' :
+            computation = previous * current;
+            break;
+        case '÷' :
+            computation = previous / current;
+            break;
+        default:
+            return;
+    }
+/*populate the value of the variables for updatedisplay method and clear off the previousOperand*/
+    
+    this.currentOperand  = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
+
+}
+
+
+/*This method keeps updating the diplay of the calculator as an when any calculation happen or any input is provided by calculator key */
+updateDisplay(){
+    if (this.currentOperand === ''){
+        this.displayElement.innerText = this.previousOperand ;
+    }
+     
+    else{
+        this.displayElement.innerText = this.currentOperand ;
+    }
+}
+}
+
+//create an instance of the calculator class //
+const calculator1 = new calculator(displayElement);
+
+//loop through all the buttons for number keys and attach an event listener that first appends the number and then updates the display//
+numberBtns.forEach(btn => {
+    btn.addEventListener('click' ,() => {
+        console.log(btn);
+         calculator1.appendNumberToDisplay(btn.innerText);
+         calculator1.updateDisplay();
+    })
+});
+//loop through all the buttons for operator keys and attach an event listener that first choose the number and then updates the display//
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click' , () => {
+        calculator1.checkOperation(btn.innerText);
+        calculator1.updateDisplay();
+    })
+})
+
+
+
+equalsBtn.addEventListener('click', () => {
+      calculator1.compute();
+      calculator1.updateDisplay();
+})
+
+allClearBtn.addEventListener('click' , () =>{
+    calculator1.allClear();
+  })
+
+
